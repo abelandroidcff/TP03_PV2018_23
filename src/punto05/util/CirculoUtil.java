5453fbe8fd87feef4239e89b0842ff785df770ff
brunoestrada <38369198+brunoestrada@users.noreply.github.com>
2018-04-18 00:49:55 -0300

    Update CirculoUtil.java
    Corrigiendo punto05.java

--- src/punto05/util/CirculoUtil.java
+++ src/punto05/util/CirculoUtil.java
@@ -1,50 +1,69 @@
 package punto05.util;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import punto05.dominio.Circulo;
 
 public class CirculoUtil {
 
-    Scanner scanner = new Scanner(System.in);
-
-    private ArrayList<Circulo> listaCirculos = new ArrayList();
+    private List<Circulo> circulos;
 
     public CirculoUtil() {
+        circulos = new ArrayList<>();
     }
 
-    public ArrayList<Circulo> getListaCirculos() {
-        return listaCirculos;
+    /**
+     * Metodo que se encarga de cargar un arreglo de circulos.
+     */
+    public void cargarTablaCirculo() {
+        circulos.add(new Circulo("circulo 01", 2.3));
+        circulos.add(new Circulo("circulo 02", 1.2));
+        circulos.add(new Circulo("circulo 03", 0.8));
+        circulos.add(new Circulo("circulo 04", 1.7));
     }
 
-    public void setListaCirculos(ArrayList<Circulo> listaCirculos) {
-        this.listaCirculos = listaCirculos;
-    }
-/*Metodo para agregar elementos al array de circulos*/
-    public void registrarCirculos() {
-        Circulo circulo = new Circulo();
-        System.out.print("Ingrese Coordenada x del Circulo: ");
-        circulo.setX(scanner.nextDouble());
-
-        System.out.print("ingrese Coordenada y del circulo: ");
-        circulo.setY(scanner.nextDouble());
-
-        System.out.print("Ingrese el Radio del Circulo: ");
-        circulo.setRadio(scanner.nextDouble());
-
-        listaCirculos.add(circulo);
-    }
-
-    /*Metodo para visualizar los elementos agregados al arraylist de circulos*/
-    public void mostrarCirculos() {
-        System.out.println("              CIRCULOS             ");
-        System.out.println("----------------------------------------------");
-        System.out.println("Coordenadas           Radio         Superficie");
-
-        for (int i = 0; i < listaCirculos.size(); i++) {
-            double auxiliarSuperficie = listaCirculos.get(i).obtenerSuperficie();
-            System.out.println("(" + listaCirculos.get(i).getX() + "," + listaCirculos.get(i).getY() + ")" + ")" + "            " + listaCirculos.get(i).getRadio() + "           " + auxiliarSuperficie);
+    /**
+     * Metodo que recorre el arreglo de circulos.
+     */
+    public void mostrarTablaCirculos() {
+        DecimalFormat df = new DecimalFormat("0.00");
+        System.out.println("\n__________LISTA DE CIRCULOS__________");
+        for (Circulo arrayCirculo : circulos) {
+            System.out.println("\nNOMBRE: " + arrayCirculo.getNombre()
+                    + "\nRADIO: " + df.format(arrayCirculo.getRadio())
+                    + "\nAREA: " + df.format(arrayCirculo.getSuperficie()));
         }
-        System.out.println("");
     }
+
+    /**
+     * Metodo que se encarga de crear un nuevo Circulo con datos ingresados por
+     * consola.
+     */
+    public void crearNuevoCirculo() {
+        try {
+            String nombre;
+            double radio;
+            Scanner scn = new Scanner(System.in);
+            System.out.println("\n__________CREAR NUEVO CIRCULO__________");
+            System.out.print("Ingrese nombre del circulo: ");
+            nombre = scn.nextLine();
+            System.out.print("Ingrese radio del circulo: ");
+            radio = scn.nextDouble();
+            circulos.add(new Circulo(nombre, radio));
+            mostrarTablaCirculos();
+        } catch (Exception e) {
+            System.out.println("El valor ingresado es incorrecto!");
+        }
+    }
+
+    public List<Circulo> getCirculos() {
+        return circulos;
+    }
+
+    public void setCirculos(List<Circulo> circulos) {
+        this.circulos = circulos;
+    }
+
 }
